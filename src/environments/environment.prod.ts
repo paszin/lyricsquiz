7a7218b882b93a55f99b302e5cb16d376c7a47ed
paszin <pascalslogin@gmail.com>
2017-08-29 00:50:21 +0200

    typo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   spotifyConfig : {
     clientId: '4591ffb8ffe34d61ad696a41881914c5',
-	redirectUri: 'http://localhost:4200/callback',
+	redirectUri: 'https://paszin.github.io/lyricsquiz/callback',
 	//authToken: localStorage.getItem('angular2-spotify-token'),
 	scope: 'playlist-read-private playlist-read-collaborative user-library-read user-read-private'
   }
