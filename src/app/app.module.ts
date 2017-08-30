2b8146f7ed79e885e574ff60de018bc126a86c89
paszin <pascalslogin@gmail.com>
2017-08-30 16:39:50 +0200

    style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-import {MdButtonModule, MdCheckboxModule, MdCardModule, MdListModule, MdIconModule} from '@angular/material';
+import {MdButtonModule, MdCheckboxModule, MdCardModule, MdListModule, MdIconModule, MdTabsModule, MdToolbarModule} from '@angular/material';
 import {FormsModule} from '@angular/forms';
 import {FlexLayoutModule} from "@angular/flex-layout";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { HttpModule }    from '@angular/http';
 
@@ -21,7 +22,7 @@ import { HeaderBarComponent } from './header-bar/header-bar.component';
     AppComponent, CallbackComponent, TrackselectionComponent, TrackComponent, LandingComponent, QuizComponent, HeaderBarComponent
   ],
   imports: [
-    BrowserModule, MdButtonModule, MdCardModule, MdCheckboxModule, MdListModule, MdIconModule, HttpModule, FormsModule, FlexLayoutModule,
+    BrowserModule, MdButtonModule, MdCardModule, MdCheckboxModule, MdListModule, MdIconModule, MdTabsModule, HttpModule, FormsModule, FlexLayoutModule, MdToolbarModule, BrowserAnimationsModule, StickyModule,
 	  RouterModule.forRoot([
       {
         path: 'app',
