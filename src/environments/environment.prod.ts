0f8ba6970453dc3396ca3265cab648396bc86949
Pascal C <pascalslogin@gmail.com>
2018-04-05 09:27:21 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   spotifyConfig : {
     clientId: '4591ffb8ffe34d61ad696a41881914c5',
-	redirectUri: 'https://paszin.github.io/lyricsquiz/callback',
+	redirectUri: 'http//guess-the-song.madeby.crenzin.de/callback',
 	//authToken: localStorage.getItem('angular2-spotify-token'),
 	scope: 'playlist-read-private playlist-read-collaborative user-library-read user-read-private'
   }
